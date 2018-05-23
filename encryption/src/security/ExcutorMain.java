62bff12984b5f323698966d397dc2afc6bfb4d35
mmm311 <1034930026@qq.com>
2018-05-23 19:33:48 +0800

    版本1 格式问题

--- encryption/src/security/ExcutorMain.java
+++ encryption/src/security/ExcutorMain.java
@@ -3,8 +3,11 @@ package security;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+/**
+ * @author liu
+ */
 public class ExcutorMain {
-    public static  void excutor(String algorithm, String inFile,String outFile, int cores, boolean flag) throws Exception{
+     private static void excutor(String algorithm, String inFile,String outFile, int cores, boolean flag) throws Exception{
         new ExcutorFile(algorithm, inFile, outFile, cores, flag).excutorFile();
     }
 
@@ -14,18 +17,17 @@ public class ExcutorMain {
         String inPath = "E:\\data\\test\\part";
         try{
             BufferedReader br = new BufferedReader(new FileReader(configure));
-            String line = null;
+            String line;
             // 参数
-            String algorithm = null;
-            String inFile = null;
-            int cores = 1;
-            boolean flag = true;
+            String algorithm, inFile;
+            int cores;
+            boolean flag;
             while ((line = br.readLine()) != null){
                 String[] params = line.split(",");
                 algorithm = params[0];
                 inFile = inPath + params[1] + "mkv";
                 cores = Integer.valueOf(params[2]);
-                flag = Integer.valueOf(params[3]) == 1? true : false;
+                flag = Integer.valueOf(params[3]) == 1 ;
                 excutor(algorithm, inFile, outFile, cores, flag);
             }
         }catch (Exception e){
